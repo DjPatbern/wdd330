36befd1a1ca3302c217fe327eb16c7ad83f4eed0
DjPatbern <victorbernardpatrick@gmail.com>
2025-05-24 18:52:33 +0100

    empty cart logic

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,9 +2,22 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const message = document.getElementById("empty-cart-message");
+  const productList = document.querySelector(".product-list");
+
+  if (!cartItems || cartItems.length === 0) {
+    message.style.display = "block";
+    productList.innerHTML = "";
+  } else {
+    message.style.display = "none";
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    productList.innerHTML = htmlItems.join("");
+  }
+
+  console.log(cartItems,"CART");
+  
 }
+
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
